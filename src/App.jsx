dc84be2561454b01ceaa1cbcc86b21100ead4e18
Ahsan <ahsansatti402@gmail.com>
2024-03-10 12:17:24 +0500

    project-starter_install-tawlind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline bg-blue-50 text-center'>
+				Hello world!
+			</h1>
+		</div>
+	);
+};
+
+export default App;
